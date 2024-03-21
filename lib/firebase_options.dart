37fb5a2f241cf7d31acaaad611735546cf155489
Xanish Aghalarov <124644409+appodll@users.noreply.github.com>
2024-03-22 03:47:37 +0400

    firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -33,39 +33,5 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBy2fH6oNkbVTKnqKBUvXp4Wba221H3FSM',
-    appId: '1:152575014517:web:7eda1dd02a1d70b64eaa3c',
-    messagingSenderId: '152575014517',
-    projectId: 'tutorialapplication-3ec17',
-    authDomain: 'tutorialapplication-3ec17.firebaseapp.com',
-    storageBucket: 'tutorialapplication-3ec17.appspot.com',
-    measurementId: 'G-RR5D2BNGB0',
-  );
-
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAsy8uYlHj-JaVFXOZleo-gwHP4LOz-FqU',
-    appId: '1:152575014517:android:341538e3b581f94c4eaa3c',
-    messagingSenderId: '152575014517',
-    projectId: 'tutorialapplication-3ec17',
-    storageBucket: 'tutorialapplication-3ec17.appspot.com',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBys2spcuW5wjcU4uTnbvsZOWoTdIeKRjk',
-    appId: '1:152575014517:ios:d4c9a6ad4961a32e4eaa3c',
-    messagingSenderId: '152575014517',
-    projectId: 'tutorialapplication-3ec17',
-    storageBucket: 'tutorialapplication-3ec17.appspot.com',
-    iosBundleId: 'com.example.applicationtutorial',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyBys2spcuW5wjcU4uTnbvsZOWoTdIeKRjk',
-    appId: '1:152575014517:ios:9f562574fa227b924eaa3c',
-    messagingSenderId: '152575014517',
-    projectId: 'tutorialapplication-3ec17',
-    storageBucket: 'tutorialapplication-3ec17.appspot.com',
-    iosBundleId: 'com.example.applicationtutorial.RunnerTests',
-  );
+  //BAXMAQ OLMAZ
 }
